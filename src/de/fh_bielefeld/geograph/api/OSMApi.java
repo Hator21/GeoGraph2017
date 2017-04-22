d1c7883961fc2fb4af3b0e8a7c4d5c558c7200c2
PhilippCl <philipp.clausing@web.de>
2017-04-22 15:40:21 +0200

    Indentation fixes to match Code Style

--- src/de/fh_bielefeld/geograph/api/OSMApi.java
+++ src/de/fh_bielefeld/geograph/api/OSMApi.java
@@ -1,34 +1,31 @@
 package de.fh_bielefeld.geograph.api;
 
 public class OSMApi {
-	
-	public static final String api_uri = "http://api.openstreetmap.org/";
-	public static final String api_test_uri = "http://api06.dev.openstreetmap.org/";
-	
-	public static enum CALLS {
-		Capabilities("/api/capabilities"),
-		BoundingBoxMap("/api/0.6/map"), //GET /api/0.6/map?bbox=left,bottom,right,top
-		Permissions("/api/0.6/permissions")
-		;
-		
-		private final String call_uri;
 
-	    /**
-	     * @param text 
-	     */
-	    private CALLS(final String call_uri) {
-	        this.call_uri = call_uri;
-	    }
-	    
-	    @Override
-	    public String toString() {
-	        return call_uri;
-	    }
-	}
-	
-	//TODO: HTTP Basic Authentication
-	
-	public OSMApi(){
-		
-	}
+    public static final String api_uri = "http://api.openstreetmap.org/";
+    public static final String api_test_uri = "http://api06.dev.openstreetmap.org/";
+
+    public static enum CALLS {
+        Capabilities("/api/capabilities"),
+        BoundingBoxMap("/api/0.6/map"), //GET /api/0.6/map?bbox=left,bottom,right,top
+        Permissions("/api/0.6/permissions")
+        ;
+
+        private final String call_uri;
+
+        private CALLS(final String call_uri) {
+            this.call_uri = call_uri;
+        }
+
+        @Override
+        public String toString() {
+            return call_uri;
+        }
+    }
+
+    //TODO: HTTP Basic Authentication
+
+    public OSMApi(){
+
+    }
 }
