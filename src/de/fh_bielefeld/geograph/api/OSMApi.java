f394a70438464165df128a3377fbb2a30aacc9b8
PhilippCl <philipp.clausing@web.de>
2017-04-23 17:06:47 +0200

    Added BoundingBox Operation to retrieve Nodes,Ways etc

--- src/de/fh_bielefeld/geograph/api/OSMApi.java
+++ src/de/fh_bielefeld/geograph/api/OSMApi.java
@@ -1,13 +1,27 @@
 package de.fh_bielefeld.geograph.api;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 public class OSMApi {
 
     public static final String api_uri = "http://api.openstreetmap.org/";
-    public static final String api_test_uri = "http://api06.dev.openstreetmap.org/";
+    public static final String api_test_uri = "http://www.openstreetmap.org";
 
     public static enum CALLS {
         Capabilities("/api/capabilities"),
-        BoundingBoxMap("/api/0.6/map"), //GET /api/0.6/map?bbox=left,bottom,right,top
+        BoundingBoxMap("/api/0.6/map?bbox="), //GET /api/0.6/map?bbox=left,down,right,up
         Permissions("/api/0.6/permissions")
         ;
 
@@ -23,16 +37,46 @@ public class OSMApi {
         }
     }
 
-    //TODO: HTTP Basic Authentication
-
-    public OSMApi(){
-
+    public static Document getBoundingBoxOfRange(double latitude, double longitude, double range) throws IOException, ParserConfigurationException, SAXException{
+        return getBoundingBoxLatLong(latitude-range, longitude-range, latitude+range, latitude+range);
     }
+
+    public static Document getBoundingBoxLatLong(double minLatitude, double minLongitude, double maxLatitude, double maxLongitude) throws IOException, ParserConfigurationException, SAXException{
+
+        DecimalFormat format = new DecimalFormat("##0.0000000", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+
+        String left = format.format(minLongitude);
+        String down = format.format(minLatitude);
+        String right = format.format(maxLongitude);
+        String up = format.format(maxLatitude);
+
+        String connectionString = api_test_uri + CALLS.BoundingBoxMap +
+                left  + "," +
+                down  + "," +
+                right + "," +
+                up;
+
+        URL apiURL = new URL(connectionString);
+        System.out.println(apiURL);
+        HttpURLConnection connection = (HttpURLConnection) apiURL.openConnection();
+
+        DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+
+        return docBuilder.parse(connection.getInputStream());
+    }
+
+
 
     //For testing purposes only! Remove when merged into Master!
     public static void main(String[] args){
         //For testing purposes only! Remove when merged into Master!
-
-        OSMApi oapi = new OSMApi();
+        Document d = null;
+        try {
+            d = OSMApi.getBoundingBoxLatLong(0.0000000,10.0000000,0.1000000,10.1000000);
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            e.printStackTrace();
+        }
+        System.out.println(d.getDocumentElement());
     }
 }
