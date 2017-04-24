464ea437f455db268a051ce99c2bbf42aa81bc9b
Stefan Ubuntu <stefan.schuck@fh-bielefeld.de>
2017-04-24 11:03:10 +0200

    Anlegen der Parser Struktur und Testdaten

---
+++ src/de/fh_bielefeld/geograph/Parser/OmlParser.java
@@ -0,0 +1,17 @@
+package de.fh_bielefeld.geograph.Parser;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import java.io.File;
+
+
+public class OmlParser {
+	
+
+    private void parse(){
+    
+    }
+    
+    public void main (String args[]){
+    }
+}
