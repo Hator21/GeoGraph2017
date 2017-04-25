5662e60221c4b4809fafca1ac44526a8794c1736
jonastogo <jonas-lampe95@web.de>
2017-04-25 22:35:45 +0200

    Koordinaten Mapping + Pane durch Canvas ersetzt

--- src/de/fh_bielefeld/geograph/GUI/OSMStreetGUIController.java
+++ src/de/fh_bielefeld/geograph/GUI/OSMStreetGUIController.java
@@ -1,10 +1,11 @@
 package de.fh_bielefeld.geograph.GUI;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
 
 public class OSMStreetGUIController {
 
@@ -14,11 +15,17 @@ public class OSMStreetGUIController {
 
 	@FXML private Slider	zoomSlider;
 
-	@FXML private Pane		mapPane;
+	@FXML private Canvas	paintingCanvas;
+
+	private GraphicsContext	gc;
+
+	private double			minLatitude	= 52, maxLatitude = 54, minLongitude = 8, maxLongitude = 10;
 
 	@FXML
 	public void initialize() {
+		gc = paintingCanvas.getGraphicsContext2D();
 
+		System.out.println(mapLongitude(9));
 		searchButton.setOnAction((event) -> {
 			String latitude = latitudeTextField.getText();
 			String longitude = longitudeTextField.getText();
@@ -28,6 +35,18 @@ public class OSMStreetGUIController {
 		zoomSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
 			System.out.println("Slider Value Changed (newValue: " + newValue.doubleValue() + ")\n");
 		});
+	}
+
+	private void drawShapes(GraphicsContext gc) {
 
 	}
+
+	public int mapLatitude(double latitude) {
+		return (int) ((paintingCanvas.getWidth() * latitude - (minLatitude * paintingCanvas.getWidth())) / (maxLatitude - minLatitude));
+	}
+
+	public int mapLongitude(double longitude) {
+		return (int) ((paintingCanvas.getHeight() * longitude - (minLongitude * paintingCanvas.getHeight())) / (maxLongitude - minLongitude));
+	}
+
 }
