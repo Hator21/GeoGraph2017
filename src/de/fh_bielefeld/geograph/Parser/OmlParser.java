c10ecd5c1cc8249c5dadfe497b934754592299dd
Stefan Schuck <Stefan Schuck>
2017-05-05 12:55:54 +0200

    added the deleteConditions and altered the parsing of the nodes

--- src/de/fh_bielefeld/geograph/Parser/OmlParser.java
+++ src/de/fh_bielefeld/geograph/Parser/OmlParser.java
@@ -13,69 +13,46 @@ import java.util.Map;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
 
 
 public class OmlParser {
     private double positiveDifference, negativeDifference;
+    private AVLTree<MapNode> parsedNodeTree;
+    private AVLTree<MapWay> parsedWayTree;
+    private ArrayList<MapNode> nodesToTransfer;
+    private ContentHolder usedHolder;
+    private Map<String,String> changedIDS;
+    private Map<String,String> deleteConditions;
     
-    OmlParser(){
+    OmlParser(ContentHolder givenHolder){
+        usedHolder = givenHolder;
         positiveDifference = 0.0000005;
         negativeDifference = positiveDifference*(-1);
+        parsedNodeTree = new AVLTree<MapNode>(usedHolder);
+        parsedWayTree = new AVLTree<MapWay>(usedHolder);
+        nodesToTransfer = new ArrayList<MapNode>();
+        changedIDS = new HashMap<String,String>();
+        deleteConditions = new HashMap<String,String>();
+        
     }
     
-     
-
-    private void parse(ContentHolder givenHolder, Document givenDocument) throws NullPointerException{
-        AVLTree<MapNode> parsedNodeTree = new AVLTree<MapNode>(givenHolder);
-        AVLTree<MapWay> parsedWayTree = new AVLTree<MapWay>(givenHolder);
+    private void setDeleteConditions(){
+        deleteConditions.put("building", "yes");
+    }
+    private void clearEverythingUnimportant(){
+        nodesToTransfer.clear();
+        nodesToTransfer.clear();
+        changedIDS.clear();
+        deleteConditions.clear();
         
-        ArrayList<MapNode> nodesToTransfer = new ArrayList<MapNode>();
-        Map<String,String> changedIDS = new HashMap<String,String>();
-        
-        NodeList nodesFromGivenDocument = givenDocument.getElementsByTagName("node");
+    }
+    private void parse(Document givenDocument) throws NullPointerException{
+        setDeleteConditions();
         NodeList waysFromGivenDocument = givenDocument.getElementsByTagName("ways");
         
-        for(int i = 0;i<nodesFromGivenDocument.getLength();i++){
-            String parsedNodeID = nodesFromGivenDocument.item(i).getAttributes().getNamedItem("id").toString();
-            Double parsedNodeLongitude = Double.parseDouble(nodesFromGivenDocument.item(i).getAttributes().getNamedItem("lon").getNodeValue());
-            Double parsedNodeLatitude = Double.parseDouble(nodesFromGivenDocument.item(i).getAttributes().getNamedItem("lat").getNodeValue());
-
-            MapNode parsedNode = new MapNode(parsedNodeID,parsedNodeLongitude,parsedNodeLatitude);
-            
-            if(nodesFromGivenDocument.item(i).hasChildNodes()==true){
-                ArrayList<MapTag> tagsFromGivenNode = new ArrayList<MapTag>();
-                NodeList childsNodesFromGivenNode = nodesFromGivenDocument.item(i).getChildNodes();
-                for(int j = 0;j<childsNodesFromGivenNode.getLength();j++){
-                    if(childsNodesFromGivenNode.item(j).hasAttributes()){
-                        String kString = nodesFromGivenDocument.item(i).getChildNodes().item(j).getAttributes().getNamedItem("k").toString();
-                        String vString = nodesFromGivenDocument.item(i).getChildNodes().item(j).getAttributes().getNamedItem("v").toString();
-                        MapTag tagFromGivenNode = new MapTag(kString,vString);
-                        tagsFromGivenNode.add(tagFromGivenNode);
-                    }
-                }
-                if(!tagsFromGivenNode.isEmpty()){
-                    parsedNode.setTagList(tagsFromGivenNode);
-                }
-            }
-
-            
-            for(int z = 0;z<nodesToTransfer.size();z++){
-                if((positiveDifference<=(parsedNodeLongitude-nodesToTransfer.get(z).getLongitude())||
-                    negativeDifference>=(parsedNodeLongitude-nodesToTransfer.get(z).getLongitude()))&&
-                   (positiveDifference<=(parsedNodeLatitude-nodesToTransfer.get(z).getLatitude())||
-                    negativeDifference>=(parsedNodeLatitude-nodesToTransfer.get(z).getLatitude()))
-                   ){
-                   changedIDS.put(parsedNodeID, nodesToTransfer.get(z).getId());
-                   nodesToTransfer.get(z).getTagList().addAll(parsedNode.getTagList());
-                   break;
-                }
-            }
-            if(!changedIDS.containsKey(parsedNode.getId())){
-                nodesToTransfer.add(parsedNode);
-            }
-        } 
-        
         for(int i = 0;i<waysFromGivenDocument.getLength();i++){
+            boolean isImportant = true;
             String parsedWayID = waysFromGivenDocument.item(i).getAttributes().getNamedItem("id").toString();
             ArrayList<String> refsFromGivenWay= new ArrayList<String>();
             ArrayList<MapTag> tagsFromGivenWay= new ArrayList<MapTag>();
@@ -83,26 +60,78 @@ public class OmlParser {
                 NodeList childsFromGivenWays = waysFromGivenDocument.item(i).getChildNodes();
                 
                 for(int j=0;j<childsFromGivenWays.getLength();j++){
-                    if(childsFromGivenWays.item(j).getNodeName()=="nd"){
-                        if(changedIDS.containsKey(childsFromGivenWays.item(j).getAttributes().getNamedItem("ref").getNodeValue())){
-                            refsFromGivenWay.add(changedIDS.get(childsFromGivenWays.item(j).getAttributes().getNamedItem("ref").getNodeValue()));
-                        }else{
-                            refsFromGivenWay.add(childsFromGivenWays.item(j).getAttributes().getNamedItem("ref").getNodeValue());
-                        }
-                    }else if (childsFromGivenWays.item(j).getNodeName()=="tag"){
+                    if (childsFromGivenWays.item(j).getNodeName()=="tag"){
                         String kString = childsFromGivenWays.item(j).getAttributes().getNamedItem("k").getNodeValue();
                         String vString = childsFromGivenWays.item(j).getAttributes().getNamedItem("v").getNodeValue();
                         tagsFromGivenWay.add(new MapTag(kString,vString));
+                        if(deleteConditions.containsKey(kString)){
+                            if(vString.equals(deleteConditions.get(j))){
+                                isImportant=false;
+                                break;
+                            }
+                        } 
                     }
-                }        
+                }
+                if(isImportant){
+                    for(int j=0;j<childsFromGivenWays.getLength();j++){
+                        if(childsFromGivenWays.item(j).getNodeName()=="nd"){
+                            parseNode(givenDocument.getElementById(childsFromGivenWays.item(j).getAttributes().getNamedItem("ref").getNodeValue));
+                            if(changedIDS.containsKey(childsFromGivenWays.item(j).getAttributes().getNamedItem("ref").getNodeValue())){
+                                refsFromGivenWay.add(changedIDS.get(childsFromGivenWays.item(j).getAttributes().getNamedItem("ref").getNodeValue()));
+                            }else{
+                                refsFromGivenWay.add(childsFromGivenWays.item(j).getAttributes().getNamedItem("ref").getNodeValue());
+                            }
+                        }
+                    }
+                }
             }
-            MapWay parsedWay=new MapWay(parsedWayID,refsFromGivenWay,tagsFromGivenWay);
-            parsedWayTree.insert(parsedWay);
+            if(isImportant){
+                MapWay parsedWay=new MapWay(parsedWayID,refsFromGivenWay,tagsFromGivenWay);
+                parsedWayTree.insert(parsedWay);
+            }
         }
         for(int i = 0;i<nodesToTransfer.size();i++){
         parsedNodeTree.insert(nodesToTransfer.get(i));
     }
-        givenHolder.setNodes(parsedNodeTree);
-        givenHolder.setWays(parsedWayTree);
+        usedHolder.setNodes(parsedNodeTree);
+        usedHolder.setWays(parsedWayTree);
+        clearEverythingUnimportant();
     }
+    private void parseNode(Element givenNode){
+        String parsedNodeID = givenNode.getAttributes().getNamedItem("id").toString();
+        Double parsedNodeLongitude = Double.parseDouble(givenNode.getAttributes().getNamedItem("lon").getNodeValue());
+        Double parsedNodeLatitude = Double.parseDouble(givenNode.getAttributes().getNamedItem("lat").getNodeValue());
+
+        MapNode parsedNode = new MapNode(parsedNodeID,parsedNodeLongitude,parsedNodeLatitude);
+            
+        if(givenNode.hasChildNodes()==true){
+            ArrayList<MapTag> tagsFromGivenNode = new ArrayList<MapTag>();
+            NodeList childsNodesFromGivenNode = givenNode.getChildNodes();
+            for(int j = 0;j<childsNodesFromGivenNode.getLength();j++){
+                if(childsNodesFromGivenNode.item(j).hasAttributes()){
+                    String kString = givenNode.getChildNodes().item(j).getAttributes().getNamedItem("k").toString();
+                    String vString = givenNode.getChildNodes().item(j).getAttributes().getNamedItem("v").toString();
+                    MapTag tagFromGivenNode = new MapTag(kString,vString);
+                    tagsFromGivenNode.add(tagFromGivenNode);
+                }
+            }
+            if(!tagsFromGivenNode.isEmpty()){
+                parsedNode.setTagList(tagsFromGivenNode);
+            }
+        }
+        for(int z = 0;z<nodesToTransfer.size();z++){
+            if((positiveDifference<=(parsedNodeLongitude-nodesToTransfer.get(z).getLongitude())||
+                negativeDifference>=(parsedNodeLongitude-nodesToTransfer.get(z).getLongitude()))&&
+               (positiveDifference<=(parsedNodeLatitude-nodesToTransfer.get(z).getLatitude())||
+                negativeDifference>=(parsedNodeLatitude-nodesToTransfer.get(z).getLatitude()))
+               ){
+                changedIDS.put(parsedNodeID, nodesToTransfer.get(z).getId());
+                nodesToTransfer.get(z).getTagList().addAll(parsedNode.getTagList());
+                break;
+            }
+        }
+        if(!changedIDS.containsKey(parsedNode.getId())){
+            nodesToTransfer.add(parsedNode);
+        }
+    } 
 }
