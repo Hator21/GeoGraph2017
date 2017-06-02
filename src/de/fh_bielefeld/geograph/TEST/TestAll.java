d164ce30013c267c4f8f4b723eccb6a2404dd510
Dstarke28 <dstarke@fh-bielefeld.de>
2017-06-02 09:59:12 +0200

    TEST Branch GUITest,ParserTest and Testsuite created

---
+++ src/de/fh_bielefeld/geograph/TEST/TestAll.java
@@ -0,0 +1,11 @@
+package de.fh_bielefeld.geograph.TEST;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+// more classes to add to Testsuite
+@SuiteClasses({OSMApiTest.class})
+public class TestAll {
+}
